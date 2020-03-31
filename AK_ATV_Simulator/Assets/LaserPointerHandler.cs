c23189498c1b3dfb51e166804ef4e5e4b29e2b67
ccfortenberry <cmnky@hotmail.com>
2020-03-31 13:52:27 -0800

    Catchup fork and add laser pointer framework

---
+++ AK_ATV_Simulator/Assets/LaserPointerHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using Valve.VR.Extras;
+
+public class LaserPointerHandler : MonoBehaviour
+{
+    public SteamVR_LaserPointer laser;
+
+    void Awake()
+    {
+        laser.PointerIn += PointerInside;
+        laser.PointerOut += PointerOutside;
+        laser.PointerClick += PointerClick;
+    }
+
+    public void PointerClick(object sender, PointerEventArgs e)
+    {
+
+    }
+
+    public void PointerInside(object sender, PointerEventArgs e)
+    {
+        Debug.Log("Pointer inside " + e.target.name);
+    }
+
+    public void PointerOutside(object sender, PointerEventArgs e)
+    {
+        Debug.Log("Pointer outside " + e.target.name);
+    }
+}
