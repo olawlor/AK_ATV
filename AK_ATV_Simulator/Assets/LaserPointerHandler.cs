b0fac3093738b2793b47c10185be73fdf6a5f15f
ccfortenberry <cmnky@hotmail.com>
2020-03-31 14:44:43 -0800

    Finish pause menu duplication process

--- AK_ATV_Simulator/Assets/LaserPointerHandler.cs
+++ AK_ATV_Simulator/Assets/LaserPointerHandler.cs
@@ -18,16 +18,38 @@ public class LaserPointerHandler : MonoBehaviour
 
     public void PointerClick(object sender, PointerEventArgs e)
     {
+        Debug.Log("Pointer clicked " + e.target.name);
+        IPointerClickHandler click = e.target.GetComponent<IPointerClickHandler>();
+        if (click == null)
+        {
+            return;
+        }
 
+        click.OnPointerClick(new PointerEventData(EventSystem.current));
     }
 
     public void PointerInside(object sender, PointerEventArgs e)
     {
         Debug.Log("Pointer inside " + e.target.name);
+        IPointerEnterHandler inside = e.target.GetComponent<IPointerEnterHandler>();
+        if (inside == null)
+        {
+            return;
+        }
+
+        inside.OnPointerEnter(new PointerEventData(EventSystem.current));
     }
 
     public void PointerOutside(object sender, PointerEventArgs e)
     {
         Debug.Log("Pointer outside " + e.target.name);
+        Debug.Log("Pointer inside " + e.target.name);
+        IPointerExitHandler outside = e.target.GetComponent<IPointerExitHandler>();
+        if (outside == null)
+        {
+            return;
+        }
+
+        outside.OnPointerExit(new PointerEventData(EventSystem.current));
     }
 }
