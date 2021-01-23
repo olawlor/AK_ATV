e23a0708a60d2c2545fa14c480614feecf14ac4b
Dr. Lawlor <lawlor@alaska.edu>
2021-01-22 20:59:53 -0900

    Load main scene by name instead of number (which can segfault).

--- AK_ATV_Simulator/Assets/Scripts/MainMenu.cs
+++ AK_ATV_Simulator/Assets/Scripts/MainMenu.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public void StartGame(){
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("procedural_terrain");
     }
 
     public void ExitGame(){
