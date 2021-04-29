7baf4186c89b6acb99bab1c733121aefdf249c67
Dylan Palmieri <dgpalmieri@gmail.com>
2021-04-28 21:23:23 -0800

    Add new scenario scripts that include the timer

---
+++ AK_ATV_Simulator/Assets/Scripts/ScenarioEndTriggerTimer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScenarioEndTriggerTimer : MonoBehaviour
+{
+    //public GameObject startTrigger;
+    public string endtext;
+    public GameObject timer;
+
+    // Start is called before the first frame update
+    //void Start()
+    //{
+
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+
+    //}
+
+    private void OnTriggerEnter(Collider other) {
+        //Debug.Log("Trigger entered by " + other.gameObject.name);
+        if (other.gameObject.name == "VehicleCoords") {
+            string finalTime = timer.GetComponent<Timer>().finalTime;
+            Debug.Log("Ending scenario for " + other.gameObject.name);
+            other.GetComponent<VehicleScenario>().Update_Scenario(endtext + finalTime);
+            //startTrigger.SetActive(true);
+            this.gameObject.SetActive(false);
+            timer.SetActive(false);
+        }
+    }
+}
---
+++ AK_ATV_Simulator/Assets/Scripts/ScenarioStartTriggerTimer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScenarioStartTriggerTimer : MonoBehaviour
+{
+    public GameObject endTrigger;
+    public GameObject timer;
+    public string scenario;
+
+    // Start is called before the first frame update
+    //void Start()
+    //{
+
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+
+    //}
+
+    private void OnTriggerEnter(Collider other) {
+        //Debug.Log("Trigger entered by " + other.gameObject.tag);
+        if (other.gameObject.tag == "Player") {
+            Debug.Log("Starting scenario for " + other.gameObject.tag);
+            other.GetComponent<VehicleScenario>().Update_Scenario(scenario);
+            endTrigger.SetActive(true);
+            timer.SetActive(true);
+            this.gameObject.SetActive(false);
+        }
+    }
+}
