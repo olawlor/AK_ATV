b5f229ce9f01883177ee4f99b957d90549dddb4c
bmtalotta <57196623+bmtalotta@users.noreply.github.com>
2021-09-28 12:49:25 -0400

    the files i want to keep

---
+++ AK_ATV_Simulator/Assets/Scripts/mission_direction.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mission_direction : MonoBehaviour
+{
+    public Transform playerTransform;
+    Vector3 objPos;
+    Vector3 dir;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        objPos = Vector3.zero;
+    }
+    void objectiveStarted(Vector3 obj){
+        objPos = obj;
+    }
+    void objectiveEnded(){
+        objPos = Vector3.zero;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+         dir.x = playerTransform.eulerAngles.y - objPos.y;
+        transform.localEulerAngles = dir;
+    }
+}
