f9188f14d6ade0ae22bd67e3a0c9072686851dba
Rose Peters <rmpeters3@alaska.edu>
2022-02-14 07:53:51 -0900

    Add script for switching vehicles

---
+++ AK_ATV_Simulator/Assets/Scripts/VehicleSwitch.cs
@@ -0,0 +1,26 @@
+﻿// To test with ATV, copy ATV_New prefab in file explorer
+// Attach this script to both vehicles VehicleCoords in hierarchy
+// otherPlayer = opposite players VehicleCoords, camera = Main Camera in CameraHUD
+
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class VehicleSwitch : MonoBehaviour
+{
+    public GameObject otherPlayer;
+    public GameObject camera1;
+    public GameObject camera2;
+
+    void OnMouseDown()
+    {
+        otherPlayer.GetComponent<ControllerKeyboard>().enabled = false;
+        camera1.SetActive(true);
+        camera2.SetActive(false);
+
+        GetComponent<ControllerKeyboard>().enabled = true;
+    }
+}
+
+
