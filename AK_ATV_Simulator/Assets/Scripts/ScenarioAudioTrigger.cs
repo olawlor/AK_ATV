2fc1af6c9333e52981df87828d0b11721433303e
Yali Wang <ywang35@alaska.edu>
2022-04-27 02:02:33 -0800

    Test bear scenario audio

---
+++ AK_ATV_Simulator/Assets/Scripts/ScenarioAudioTrigger.cs
@@ -0,0 +1,15 @@
+﻿/* Audio trigger for scenarios */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class ScenarioAudioTrigger : MonoBehaviour
+{
+    public AudioSource playSound;
+    void OnTriggerEnter(Collider other)
+    {
+        playSound.Play();
+    }
+}
