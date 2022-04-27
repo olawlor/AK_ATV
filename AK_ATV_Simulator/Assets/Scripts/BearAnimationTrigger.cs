c984f9dc2ce761322d2cc206fc86c7007b2701ad
Yali Wang <ywang35@alaska.edu>
2022-04-27 14:29:41 -0800

    Scenario audio trigger works, audio works

---
+++ AK_ATV_Simulator/Assets/Scripts/BearAnimationTrigger.cs
@@ -0,0 +1,17 @@
+﻿/* Used for triggering the scenario animations. 
+   Once the user hit the scenario trigger, 
+   the corresponding animation should begin to work.
+
+   Using instructions:
+   Attach this script to a GameObject with an Animator component attached.
+   
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BearAnimationTrigger : MonoBehaviour
+{
+    
+}
